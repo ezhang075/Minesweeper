4c5d95dc5ec2f6fbaa918579e0d3b3de9e57a066
ezhang075 <edwardzhangusa12@gmail.com>
2024-08-15 01:42:50 -0700

    Update MyAI.py
    Working on draft AI

--- MyAI.py
+++ MyAI.py
@@ -14,6 +14,7 @@
 
 from AI import AI
 from Action import Action
+from collections import Counter
 
 
 class MyAI( AI ):
@@ -23,6 +24,37 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
+
+		self._rowDimension = rowDimension
+		self._colDimension  = colDimension
+		self._totalMines = totalMines
+		self._startX = startX
+		self._startY = startY
+		self._mineX = 0
+		self._mineY = 0
+
+		# Stores V and C tiles
+		# C are uncovered tiles next to a covered unmarked tile
+		# V are covered tiles next to an uncovered tile
+		self.V = []
+		self.C = []
+		
+		self.warnings = []
+		self._numOfWarnings = 0
+
+		
+		self.uncovered = set([(startX, startY)])
+		self.safeTiles = set()
+
+		self._numberOfUncoveredTiles = 0
+		self._flags = False
+
+		self._recentX = startX
+		self._recentY = startY
+		self.board = [['x' for _ in range(rowDimension)] for _ in range(colDimension)]
+		self.board[startX][startY] = 0
+
+
 		pass
 		########################################################################
 		#							YOUR CODE ENDS							   #
@@ -34,7 +66,147 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
-		return Action(AI.Action.LEAVE)
+
+		# number is the number of the uncovered tile
+		
+		# gets all adjacent, in bound tiles to the coordinates
+		def get_adjacent_tiles(x, y):
+			adjacentTiles = []
+			# Top Left
+			if x-1 >= 0 and y + 1 < self._rowDimension:
+				adjacentTiles.append((x-1, y+1))
+			# Top Middle
+			if y + 1 < self._rowDimension:
+				adjacentTiles.append((x, y+1))
+			# Top Right
+			if x+1 < self._colDimension and y + 1 < self._rowDimension:
+				adjacentTiles.append((x+1, y+1))
+			# Middle Left
+			if x-1 < self._colDimension:
+				adjacentTiles.append((x-1, y))
+			# Middle Right
+			if x+1 < self._colDimension:
+				adjacentTiles.append((x+1,y))
+			# Bottom Left
+			if x-1 >= 0 and y - 1 >= 0:
+				adjacentTiles.append([x-1, y-1])
+			# Bottom Middle
+			if y - 1 >= 0:
+				adjacentTiles.append((x, y-1))
+			# Bottom Right
+			if x + 1 < self._colDimension and y - 1 >= 0:
+				adjacentTiles.append((x+1, y-1))
+			return adjacentTiles
+		
+
+		############## PRE-PROCESSING ##############
+
+		# simple algorithm: adds tiles into V and C
+		def simple_add_frontier(self):
+			for x in range(self._rowDimension):
+				for y in range(self._colDimension):
+					if self.board[x][y] == 'x':
+						continue
+
+					elif (x,y) in self.uncovered and self.board[x][y] != 'x':
+						adjacentTiles = self.get_adjacent_tiles(x,y)
+						for adj_x, adj_y in adjacentTiles:
+							if (adj_x, adj_y) not in self.uncovered and (adj_x, adj_y) not in self.warnings:
+								if (x,y) not in self.C:
+									self.C.append((x,y))
+					
+					elif (x,y) not in self.uncovered and self.board[x][y] != 'x':
+						adjacentTiles = self.get_adjacent_tiles(x,y)
+						for adj_x, adj_y in adjacentTiles:
+							if (adj_x, adj_y) not in self.uncovered and (adj_x, adj_y) not in self.warnings:
+								if (x,y) not in self.V:
+									self.V.append((x,y))
+
+		# advanced algorithm: add tiles into V and C, also computes C(v) and V(c)
+		def advanced_add_frontier(self):
+			for x in range(self._rowDimension):
+				for y in range(self._colDimension):
+					if self.board[x][y] == 'x':
+						continue
+
+					elif (x,y) in self.uncovered and self.board[x][y] != 'x':
+						adjacentTiles = self.get_adjacent_tiles(x,y)
+						for adj_x, adj_y in adjacentTiles:
+							if (adj_x, adj_y) not in self.uncovered and (adj_x, adj_y) not in self.warnings:
+								if (x,y) not in self.C:
+									self.C.append((x,y))
+								if (adj_x, adj_y) not in self.V:
+									self.V.append((adj_x, adj_y))
+									self.C_v[(adj_x, adj_y)] = []
+								if (x,y) not in self.V_c:
+									self.V_c[(x,y)] = []
+								if (adj_x, adj_y) not in self.V_c[(x,y)]:
+									self.V_c[(x,y)].append((adj_x, adj_y))
+								if (x,y) not in self.C_v[(adj_x, adj_y)]:
+									self.C_v[(adj_x, adj_y)].append((x,y))
+
+			self.V.sort(key=lambda v:len(self.C_v[v]))
+			
+
+		# checks the var assignments with the board
+		def checkVarAssignment(v):
+			for c in self.C_v[v] :
+				u = 0
+				mine_count = 0
+				for neighbor in self.V_c[c] :
+					if neighbor.value == None :
+						u+=1
+
+					if u == 1:
+						mine_count += 1
+
+					if u == 0 :
+						continue
+
+		
+		
+		# code for minimal AI 5x5
+		if self._rowDimension == 5 and self._colDimension == 5:
+			
+			# Update the returned value of tile
+			self.board[self._recentX][self._recentY] = number
+
+			# iterate through board, finding safe tiles and adding them to the safeTiles set
+			for x in range(self._rowDimension):
+				for y in range(self._colDimension):
+					if self.board[x][y] != 'x':
+						# the tile is uncovered
+						adjacentTiles = get_adjacent_tiles(x, y)
+						if self.board[x][y] == 0:
+							# the tile is 0, so we know the neighbors are safe
+							for (tileX, tileY) in adjacentTiles:
+								# if it is covered, add it to the sets if it has not been uncovered already
+								if self.board[tileX][tileY] == 'x' and 0 <= tileX < self._rowDimension and 0<= tileY < self._colDimension and (tileX, tileY) not in self.uncovered:
+									self.safeTiles.add((tileX, tileY))
+									self.uncovered.add((tileX, tileY))
+						# currently no check for if the tile is 1, ignores
+			
+			# make the moves we know are safe
+			if self.safeTiles:
+				# pops the safe tile, saves the coordinates, and uncovers
+				tile = self.safeTiles.pop()
+				self._recentX = tile[0]
+				self._recentY = tile[1]
+				return Action(AI.Action.UNCOVER, tile[0], tile[1])
+			
+			# move randomly, if no safe moves can be made
+			for x in range(self._rowDimension):
+				for y in range(self._colDimension):
+					# if a spot is covered, uncover it
+					if self.board[x][y] == 'x' and (x,y) not in self.uncovered:
+						self.uncovered.add((x,y))
+						self._recentX = x
+						self._recentY = y
+						return Action(AI.Action.UNCOVER, x, y)
+					
+			return Action(AI.Action.LEAVE)
+
+		
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
